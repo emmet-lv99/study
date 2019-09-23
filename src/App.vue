8586f0bf5c5576f1549719817b3a79ecdb559321
emmet-lv99 <dachanisfull@gmail.com>
2019-09-23 15:47:26 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <respons-page-main/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import ResponsPageMain from './views/ResponsPage.vue'
+  
 export default {
-  name: 'app',
+  name:'app',
   components: {
-    HelloWorld
-  }
+    ResponsPageMain
+  }  
 }
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/views/ResponsPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="Respons-page-wrap">
+        <p>가나다라마바사</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+    p{
+        font-size: 50px;
+        color:black;
+    }
+</style>
